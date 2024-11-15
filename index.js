f328a1ea0dc390f733381d9ab78508b8ae4c8028
zenimaniprima <sidnazayn11@gmail.com>
2024-11-15 16:45:09 +0700

    add connection test on start

--- index.js
+++ index.js
@@ -9,31 +9,39 @@ const routerIndex = express.Router();
 const port = 3005;
 const host = '0.0.0.0';
 
-const cors_setting = {
-  origin: 'https://imaniprima.co.id',
-  optionsSuccessStatus: 200,
-};
+const allowedOrigins = [
+  'https://imaniprima.co.id',
+  'http://localhost:3001',
+  'http://localhost:3000',
+];
 
-const allowedOrigins = ['https://imaniprima.co.id','http://localhost:3001','http://localhost:3000'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 app.use(bodyParser.json());
 
 routerIndex.get('/test', async (req, res) => {
   const result = await knex.raw('select 1+1 as result');
   res.status(200).send({ message: 'success', data: 'success' });
 });
+
 app.use('/api', router);
 app.use('/api', routerIndex);
 
-app.listen(port, host, () =>
-  console.log(`app is running in ${host}:${port} ${process.env.NODE_ENV}`)
-);
+app.listen(port, host, async () => {
+  console.log(`app is running in ${host}:${port} ${process.env.NODE_ENV}`);
+  const result = await knex.raw('select 1+1 as result');
+  if (result[0][0].result === 2) {
+    console.log('db connection success');
+  } else {
+    console.log('db connection failed');
+  }
+});
